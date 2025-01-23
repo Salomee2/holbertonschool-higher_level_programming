c61669188bc0a6d8bdc43b92ccb348f329493934
root <pertonsalomee@gmail.com>
2025-01-23 19:45:21 +0000

    fix task

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers in a matrix.
-    
-    Args:
-        matrix (list of lists): 2D array of integers.
-
-    Returns:
-        list of lists: New matrix with squared values.
-    """
     return [[x ** 2 for x in row] for row in matrix]
