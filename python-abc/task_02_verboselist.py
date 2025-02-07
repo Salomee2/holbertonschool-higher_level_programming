4f3db101bba3b11e07b4a83e86bc2e969cb8cff3
root <pertonsalomee@gmail.com>
2025-02-07 14:05:46 +0000

    Task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class VerboseList(list):
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
+
+    def remove(self, item):
+        if item in self:
+            super().remove(item)
+            print(f"Removed [{item}] from the list.")
+        else:
+            print(f"Item [{item}] not in the list.")
+
+    def pop(self, index=-1):
+        if self:
+            item = super().pop(index)
+            print(f"Popped [{item}] from the list.")
+            return item
+        else:
+            print("Can't pop from an empty list.")
+            return None
+
