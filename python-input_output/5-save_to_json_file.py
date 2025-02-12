506a88126deb138a2d82f5783fac3bef513f3699
root <pertonsalomee@gmail.com>
2025-02-12 18:23:16 +0000

    fix task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,26 +4,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-    Save an object as a JSON string in a file.
+    Writes an object to a text file using a JSON representation.
     """
     with open(filename, 'w') as file:
         json.dump(my_obj, file)
-
-
-my_list = [1, 2, 3]
-save_to_json_file(my_list, "my_list.json")
-
-my_dict = {
-    'id': 12,
-    'name': "John",
-    'places': ["San Francisco", "Tokyo"],
-    'is_active': True,
-    'info': {'age': 36, 'average': 3.14}
-}
-save_to_json_file(my_dict, "my_dict.json")
-
-try:
-    my_set = {132, 3}
-    save_to_json_file(my_set, "my_set.json")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
