4e7eece88f3f94f8061809d6a44ff38fe9db9d5d
root <pertonsalomee@gmail.com>
2025-03-05 16:25:28 +0000

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,8 @@
--- 1. Suppression de l'utilisateur s'il existe
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
+-- 1. Création de l'utilisateur si il n'existe pas déjà
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- 2. Création de l'utilisateur avec un mot de passe
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- 3. Attribution de tous les privilèges à l'utilisateur 'user_0d_1'
+-- 2. Attribution de tous les privilèges à cet utilisateur
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- 4. Appliquer les privilèges
+-- 3. Appliquer les privilèges
 FLUSH PRIVILEGES;
