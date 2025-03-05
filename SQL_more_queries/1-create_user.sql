469488dd38f9d87b9c81eb2327c8ef881e10145a
root <pertonsalomee@gmail.com>
2025-03-05 16:21:15 +0000

    fix 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,11 @@
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+-- 1. Suppression de l'utilisateur s'il existe
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 
+-- 2. Création de l'utilisateur avec un mot de passe
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- 3. Attribution de tous les privilèges à l'utilisateur 'user_0d_1'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- 4. Appliquer les privilèges
+FLUSH PRIVILEGES;
