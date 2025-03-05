2f26c2e75050e90ff0f7aedc295b12b211dd7377
root <pertonsalomee@gmail.com>
2025-03-05 16:28:30 +0000

    tests

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,4 @@
+-- 1. Creation de la table force_name si elle n'existe pas deja
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,4 @@
+-- 1. Création de la table id_not_null si elle n'existe pas déjà
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
