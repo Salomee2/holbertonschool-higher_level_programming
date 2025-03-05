5317972bddc4d6876cdb24c28571e1a9d15be0c3
root <pertonsalomee@gmail.com>
2025-03-05 16:18:37 +0000

    fix task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,6 @@
--- 1. Vérification si l'utilisateur 'user_0d_1' existe déjà
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- 2. Création de l'utilisateur 'user_0d_1' avec le mot de passe
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- 3. Attribution de tous les privilèges à l'utilisateur 'user_0d_1'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- 4. Appliquer les privilèges
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
+
