778667dfc2e409e49ed91546d912533c607c1f0d
root <pertonsalomee@gmail.com>
2025-03-05 16:57:43 +0000

    fix 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,14 @@
--- 6. States
--- Create a table 'states' in the database with columns 'id' and 'name'.
+-- 6. States table
+-- Create the database hbtn_0d_usa and the table states in it
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the table states if it doesn't exist
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
