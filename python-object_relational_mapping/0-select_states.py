78597c13925b265fd309bd2ba37e7d8322e1fa21
root <pertonsalomee@gmail.com>
2025-03-07 20:48:13 +0000

    task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Script qui liste tous les états de la table 'states' d'une base MySQL.
+"""
+
 import sys
-
-def list_databases():
-    """Liste toutes les bases de données d'un serveur MySQL"""
-    # Récupérer les arguments passés en ligne de commande
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Connexion au serveur MySQL
-    connection = MySQLdb.connect(host="localhost", user=user, passwd=password, db=db_name)
-
-    cursor = connection.cursor()
-    cursor.execute("SHOW DATABASES")
-
-    # Récupérer toutes les bases de données
-    databases = cursor.fetchall()
-
-    # Afficher les bases de données
-    for database in databases:
-        print(database)
-
-    cursor.close()
-    connection.close()
+import MySQLdb
 
 if __name__ == "__main__":
-    list_databases()
+	# Recuperer les arguments passes au script
+	# sys.argv[1] = username, sys.argv[2] = password, sys.argv[3] = database
+	db = MySQLdb.connect(
+		host="localhost",
+		user=sys.argv[1],
+		passwd=sys.argv[2],
+		db=sys.argv[3],
+		port=3306
+	)
 
+	# Creer un curseur pour executer des requetes SQL
+	cursor = db.cursor()
+
+	# Executer la requete pour recuperer tous les etats tries par id
+	cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+	# Recuperer et afficher les resultats
+	for row in cursor.fetchall():
+		print(row)
+
+	# Fermer le curseur et la connexion
+	cursor.close()
+	db.close()
